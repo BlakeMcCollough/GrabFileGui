855673e9988bedafc5436e14fd2e1e5248611cd7
Blake McCollough <Blake.McCollough@smithtech.com>
2019-05-30 13:53:58 -0400

    Added disk seize tab

---
+++ GrabFileGui/DiskUsageLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrabFileGui
+{
+    class DiskUsageLog //this effectively works as a C struct
+    {
+        public int Sec { get; set; }
+        public string TSK { get; set; }
+        public string Seize { get; set; }
+        public string Queue { get; set; }
+        public string App { get; set; }
+        public string IAR { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}
--- GrabFileGui/MainWindow.xaml.cs
+++ GrabFileGui/MainWindow.xaml.cs
@@ -24,43 +24,59 @@ namespace GrabFileGui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool taskRunning; //makes sure that when TaskList is loaded again by mistake, it won't start a new work thread
         private bool stopped;
         private int delay;
         private CancellationTokenSource killDelay;
 
         public MainWindow()
         {
+            taskRunning = false;
             stopped = false;
             delay = 1000;
             killDelay = new CancellationTokenSource(); //kills the delay when next is pressed
             InitializeComponent();
         }
 
+        //ESSENTIALLY works as main, responsible for waiting a second and updating the tasks
         private async void TaskList_Loaded(object sender, RoutedEventArgs e) //I've added async here so that this runs asynchronously, meaning I can use Task.Delay without shutting down the UI
         {
+            if(taskRunning == true)
+            {
+                return;
+            }
+            taskRunning = true;
+
             const string PATH = "../../GRAB.190509.123741";
-            const int LENGTH = 13;
+            const int LENGTH_TSK = 13;
+            const int LENGTH_DSK = 10;
             const int SEC = 1;
             const int TSK = 0, CLNT = 1, APP = 2, VER = 3, IAR = 4, CK = 5, SVC = 6, CPU = 7, FILE = 8, KEY = 9, DA = 10, RD = 11, WR = 12; //corresponds to index in task list
+            const int DTSK = 2, SEIZE = 4, QUEUE = 6, DAPP = 7, DIAR = 8, TIME = 9; //corresponds to index in disklog list
 
             //checks for three numbers, 12 of any characters, any word (including .) and the rest of the data entry in the format of the grab file
             //the purpose is to check if the line read is a data entry
             Regex tskAcceptRegex = new Regex(@"^\d{3} .{12} \w* +(\w|\\.)* +\w{4} +\w\w +\w\w +\d\d:\d\d:\d\d:\d\d\d +\w* +\w{8} +\w{8} \w{8} +\w{8} *$");
             //similar to tskAccept, but it checks if it's a header to a new second
-            Regex stepAcceptRegex = new Regex(@"^[\d\d Task info \d+]");
+            Regex stepAcceptRegex = new Regex(@"^\[\d\d Task info \d+]$");
+            //similar to tskAccept, but checks for disk seize header
+            Regex diskAcceptRegex = new Regex(@"^\[\d\d Disk seize]$");
 
 
             StreamReader infile = new StreamReader(PATH);
             string line = infile.ReadLine();
-            int secondCounter = 0;
+            int secondCounter = 1;
             double netUsage = 0;
 
             List<Task> runningTasks = new List<Task>(); //list of task objects to be displayed by DataGrid
+            List<DiskUsageLog> diskLogs = new List<DiskUsageLog>();
             List<string> usageQuery = new List<string>(); //if CPU changed for a task, its TSK index is stored here in an attempt to reduce time complexity
             TaskList.ItemsSource = runningTasks;
+            DiskList.ItemsSource = diskLogs;
+
             while (line != null)
             {
-                if(tskAcceptRegex.IsMatch(line) == true) //is an acceptable entry
+                if (tskAcceptRegex.IsMatch(line) == true) //is an acceptable entry
                 {
                     //since client should contain any character, it cannot be tokenized by whitespace. Hardcoding instead
                     string tskLine = line.Substring(0, 3);
@@ -71,7 +87,7 @@ namespace GrabFileGui
                     List<string> taskElements = returnedSplitList.OfType<string>().ToList(); //fancy way of transforming string array to list
                     taskElements.Insert(0, clientLine);
                     taskElements.Insert(0, tskLine);
-                    if (taskElements.Count != LENGTH)
+                    if (taskElements.Count != LENGTH_TSK)
                     {
                         taskElements.Insert(8, " ");
                     }
@@ -129,18 +145,18 @@ namespace GrabFileGui
                 }
                 else if(stepAcceptRegex.IsMatch(line) == true && runningTasks.Count != 0) //assumes that each second runs in order
                 {
-                    Console.WriteLine(netUsage);
                     foreach(string index in usageQuery)
                     {
                         Task currentTask = runningTasks.Find(x => x.TSK == index);
                         if(currentTask != null)
                         {
-                            currentTask.CPU = String.Concat(Math.Round(((currentTask.CPUTime).TotalSeconds / netUsage * 100), 2).ToString(), "%"); //calculates each % of total CPU usages: divides the current task's CPU use by sum of all CPU uses
+                            currentTask.CPU = String.Concat(Math.Round(((currentTask.CPUTime).TotalSeconds * 100), 2).ToString(), "%"); //doesn't calculate anything anymore, just a % rep of seconds difference
                             runningTasks.Remove(currentTask);
                             runningTasks.Insert(0, currentTask);
                         }
                     }
                     TaskList.Items.Refresh();
+                    DiskList.Items.Refresh();
 
                     do
                     {
@@ -161,6 +177,25 @@ namespace GrabFileGui
                     usageQuery.Clear();
                     netUsage = 0;
                     secondCounter = secondCounter + 1;
+                }
+                else if(diskAcceptRegex.IsMatch(line) == true)
+                {
+                    line = infile.ReadLine(); //we're doing a readline here since the next line will (hopefully) be disk stuff
+                    string[] returnedSplitList = Regex.Split(line, @" +|,|=");
+                    List<string> diskElements = returnedSplitList.OfType<string>().ToList(); //fancy way of transforming string array to list
+                    if(diskElements.Count == LENGTH_DSK)
+                    {
+                        diskLogs.Add(new DiskUsageLog()
+                        {
+                            Sec = secondCounter,
+                            TSK = diskElements[DTSK],
+                            Seize = diskElements[SEIZE],
+                            Queue = diskElements[QUEUE],
+                            App = diskElements[DAPP],
+                            IAR = diskElements[DIAR],
+                            TimeStamp = diskElements[TIME]
+                        });
+                    }
                 }
 
 
@@ -214,6 +249,12 @@ namespace GrabFileGui
                     SpeedBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
                 }
             }
+        }
+
+
+        private void DiskList_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Console.WriteLine("Loaded disklist gui");
         }
     }
 }
